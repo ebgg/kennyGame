eaaa897b79cc18bfe214459c5e2a8ad0cafbf27f
ebgg <ebgg1997@gmail.com>
2020-04-18 16:58:38 +0300

    Initial Commit

---
+++ kennyGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kennyGame
+//
+//  Created by Erhan Büyükgölcigezli on 18.04.2020.
+//  Copyright © 2020 Erhan Büyükgölcigezli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
